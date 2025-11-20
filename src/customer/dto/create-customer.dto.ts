5b7f368ecc910b3be700218f43529562975af366
Tanjim Siam <siam88536@gmail.com>
2025-11-20 16:46:44 +0600

    validation pipes for dto

--- src/customer/dto/create-customer.dto.ts
+++ src/customer/dto/create-customer.dto.ts
@@ -1,4 +1,8 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+import { IsInt, IsString } from 'class-validator';
 export class CreateCustomerDto {
+  @IsString()
   name: string;
+  @IsInt()
   age: number;
 }
