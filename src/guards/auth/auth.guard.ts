7be52e6dabae9d3ad652281950ab804be15faa37
Tanjim Siam <siam88536@gmail.com>
2025-11-21 08:25:39 +0600

    Protecting Routes using Guards

---
+++ src/guards/auth/auth.guard.ts
@@ -0,0 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext,
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authHeader = request.headers['authorization'];
+    return authHeader === 'Bearer valid-token';
+  }
+}
